979189b3c66b482d4c8db609c92cff4923065817
raniaruna <raniaruna2005@gmail.com>
2017-08-08 13:45:47 -0700

    wk2 P4
    week 2 P4

--- server.js
+++ server.js
@@ -4,9 +4,61 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var articles = {
+	'article-one' : {
+		title: 'Article One | Rani',
+		date:	'Aug 1 2017',
+		heading: 'Learn WebApp',
+		content: `<p>Learning Web App developemnt through IMAD 8 week program</p>
+				  <p>Every week you get	video tutorials regarding different topics </p>	`
+	},
+	'article-two' : {
+		title:'Article Two | Rani',
+		date:'Aug 4 2017',
+		heading:'Weekly assignments',
+		content:`<p>Learning Web App developemnt through IMAD 8 week program</p>
+				  <p>Every week submit assignment reading that week subject </p>	`
+	},
+	'article-three' :{
+		title:'Article Three | Rani',
+		date:'Aug 4 2017',
+		heading:'Final Exam',
+		content:`<p>Learning Web App developemnt through IMAD 8 week program</p>
+				  <p>End of cource you can take Final exam and get the certificate </p>	`
+	},
+
+	};
+
+function createTemplate(data){
+	
+	var title = data.title;
+	var date =data.date;
+	var content =data.content;
+	var heading =data.heading;
+	var htmlTemplate =`<html>
+    <head>
+        <link href="/ui/style.css" rel="stylesheet" />
+		<title>${title}</title>
+    </head>
+    <body>
+        <div class="container">
+		<div><h1>${heading} </h1></div>
+		<div>${date} </div>
+		<div><p>${content} <p></div>
+		<div>
+    </body>
+</html>`;
+return htmlTemplate;
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+app.get('/:articleName', function (req, res) {
+	var articleName =req.params.articleName
+	
+
+  res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
