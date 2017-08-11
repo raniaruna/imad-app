1a4dbb9a19089055c6d1036f2873386a5dc279fa
raniaruna <30811229+raniaruna@users.noreply.github.com>
2017-08-11 15:09:00 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var Pool = require('pg').Pool;
 var config = {
     user:'raniaruna2005',
     database:'raniaruna2005',
-    host:'http://db.imad.hasura-app.io',
+    host:'db.imad.hasura-app.io',
     port:'5432',
     password:process.env.DB_PASSWORD
 };
