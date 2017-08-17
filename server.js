e4e416961571bf045f5096b42fd32b1ee2810f18
raniaruna <30811229+raniaruna@users.noreply.github.com>
2017-08-16 22:26:17 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ function hash(input,salt){
      var username = req.body.username;
      var password = req.body.password;
      var dbString = hash(password,salt);
-    pool.query('INSERT INTO "USER"(USERNAME,PASSWORD) VALUES($1,$2)',[username,dbString] ,function(err,result){
+    pool.query('INSERT INTO "USER" (USERNAME,PASSWORD) VALUES($1,$2)',[username,dbString] ,function(err,result){
 	    if(err){
 	        res.status(500).send(err.toString());
 	    } else {
