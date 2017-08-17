ca6be1140abd2fe2bc778f99a2f92b5b5db9646f
raniaruna <30811229+raniaruna@users.noreply.github.com>
2017-08-17 12:44:10 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -66,9 +66,9 @@ app.get('/hash/:input',function(req,res){
      var password = req.body.password;
      var salt = crypto.randomBytes(128).toString('hex');
      var dbString = hash(password,salt);
-          console.log('in server creating user');
-     
-    pool.query('INSERT INTO "user" (username,password) VALUES($1,$2)',[username,dbString] ,function(err,result){
+          console.log('in server creating user' +username+ ", "+password);
+    
+    pool.query('INSERT INTO "user" ("username","password") VALUES($1,$2)',[username,dbString] ,function(err,result){
 	    if(err){
 	        res.status(500).send(err.toString());
 	    } else {
