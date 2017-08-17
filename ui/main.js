f752c4e4ee528ff63f782427dca48c60cdb73f2c
raniaruna <30811229+raniaruna@users.noreply.github.com>
2017-08-17 12:15:51 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -43,12 +43,46 @@ button.onclick=function(){
 	req.send(null);
 };
 
-var create_submit = document.getElementById("create-submit-btn");
 
-create_submit.onclick = function(){
+ var register = document.getElementById('register_btn');
+    register.onclick = function () {
+        // Create a request object
+        var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  alert('User created successfully');
+                  register.value = 'Registered!';
+              } else {
+                  alert('Could not register the user');
+                  register.value = 'Register';
+              }
+          }
+        };
+        
+        // Make the request
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        console.log(username);
+        console.log(password);
+        request.open('POST', '/create-user', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+        register.value = 'Registering...';
+    
+    };
+
+
+
+var register = document.getElementById("register_btn");
+
+register.onclick = function(){
 //create req obj
  alert('create clicked');
-	var req = new XMLHttpRequest();
+	var request = new XMLHttpRequest();
 
 	//capture response and store in variable 
 	req.onreadystatechange =function(){
