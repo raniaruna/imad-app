ad46ad4d59570a5bcb036c4e485e11b3a482d110
raniaruna <30811229+raniaruna@users.noreply.github.com>
2017-08-16 21:33:03 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var crypto =require('crypto');
 var Pool = require('pg').Pool;
 var config = {
     user:'raniaruna2005',
@@ -39,6 +40,17 @@ var counter =0;
 app.get('/counter', function (req, res) {
 	counter++;
   res.send(counter.toString());
+});
+
+function hash(input,salt){
+    var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return hashed.toString('hex');
+}
+    
+}
+app.get('/hash/:input',function(req,res){
+    var hashedStr = hash(req.params.input,'this-is-some-random-string');
+    res.send(hashedStr);
 });
 // get data from database 
 app.get('/tb_test', function (req, res) {
