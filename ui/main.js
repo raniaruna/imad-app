cbde2f875e57f350995e5be7ffe52aa92ed9c5af
raniaruna <30811229+raniaruna@users.noreply.github.com>
2017-08-18 15:08:42 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,40 +1,6 @@
 console.log('Loaded!');
 
-function loadCommentForm(){
-    var commentHTML =` <h3>Add Cooment </h3><input type='text' id='comment' placeholder='Add comment'/><br/>
-                    
-                    <input type='submit' id='cooment_btn' value='Save'/>` ;
-    var commentForm = document.getElementById("cooment_form");
-    commentForm.innerHTML = commentHTML;
-    
-}
-function loadComments(articleName){
-    alert("getting comments for article");
-    var request = new XMLHttpRequest();
-if(request.readyState== XMLHttpRequest.DONE){
-    var comments = document.getElementById("comments");
-		   
-			if(request.status===200){
-			    
-			    var content = '<ul>';
-			    var commentData =JSON.parse(request.responseText);
-			    for(var i=0; i< commentData.length; i++){
-			        content +=`<li>
-			                ${commentData[i].comment}
-			                 - (${commentData[i].timestamp.split('T')[0]})</li>`;
-			    }
-			    content +='</ul>';
-			    comments.innerHTML =content;
-			    
-			} else {
-			   comments.innerHTML ='Could not load all Comments!';
-			  
-			}
-		}
-	request.open('GET','/get-comments/'+articleName,true);
-	request.setRequestHeader('Content-Type','application/json');
-	request.send(null);
-}
+
 function loadLoginForm(){
     var loginHTML =` <h3>Login User </h3><input type='text' id='username' value='newuser2'/><br/>
                     <input type='password' id='password' value='password'/><br/>
