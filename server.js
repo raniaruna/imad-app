69e94dd33519660daa78113b01a50f4b37e4e1bf
raniaruna <30811229+raniaruna@users.noreply.github.com>
2017-08-17 21:07:37 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ app.post('/login',function(req,res){
      var username = req.body.username;
      var password = req.body.password;
      console.log('login in server '+username+ ', '+password);
-    pool.query('SELECT  ID,USERNAME,PASSWORD FROM "USER" where username=$1 ',[username] ,function(err,result){
+    pool.query('SELECT  ID,USERNAME,PASSWORD FROM "user" where username=$1 ',[username] ,function(err,result){
 	    if(err){
 	        res.status(500).send(err.toString());
 	    } else {
