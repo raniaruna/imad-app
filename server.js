53a3b9b6f22f96ef9cf5eda6c997861063868b40
raniaruna <30811229+raniaruna@users.noreply.github.com>
2017-08-17 22:44:29 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -140,8 +140,8 @@ app.get('/get-articles', function (req, res) {
 });
 app.get('/get-comments/:articleName', function (req, res) {
     var articleName =req.params.articleName;
-	pool.query(`SELECT comment.*,user.username FROM comment,article,user where article.title =$1 and comment.user_id =user.id and comment.article_id = article.id 
-	               ORDER BY comment.timestamp DESC `,[articleName],function(err,result){
+	pool.query(`SELECT comment.*,"user".username FROM article, comment,"user" WHERE article.title =$1 AND  comment.user_id ="user".id 
+                AND article.id =comment.article_id  ORDER BY comment.timestamp DESC `,[articleName],function(err,result){
 	    if(err){
 	        res.status(500).send(err.toString());
 	    } else {
