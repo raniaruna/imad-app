6f85ea1b8a201f86b1af7d2f96cbe4988ae03491
raniaruna <30811229+raniaruna@users.noreply.github.com>
2017-08-17 20:04:35 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -46,13 +46,18 @@ button.onclick=function(){
 
 
 function loadLoginForm(){
-    var loginHTML =` <h3>Register User </h3><input type='text' id='username'/><br/>
+    var loginHTML =` <h3>Login User </h3><input type='text' id='username'/><br/>
+                    <input type='password' id='password'/><br/>
+                    <input type='submit' id='login_btn' value='Register'/><br/><spam id='txt-result'></spam>`;
+   var registerHTML =` <h3>Register User </h3><input type='text' id='username'/><br/>
                     <input type='password' id='password'/><br/><input type='text' id='user_name'/><br/><input type='text' id='user_email'/><br/>
                     <input type='submit' id='register_btn' value='Register'/><br/><spam id='txt-result'></spam>`;
                     
     var login =document.getElementById("login_area");
                     login.innerHTML = loginHTML;
                     console.log('here 222');
+                    
+                    
 var register = document.getElementById("register_btn");
 
 register.onclick = function(){
@@ -89,7 +94,43 @@ register.onclick = function(){
 	request.setRequestHeader('Content-Type','application/json');
 	request.send(JSON.stringify({username: username, name:user_name, email:user_email, password: password}));
 	register.value='Registering ...'
-};
+}; // register end
+var loginBtn = document.getElementById("login_btn");
+
+loginBtn.onclick = function(){
+//create req obj
+ alert('login clicked');
+ var textRes = document.getElementById("txt-result");
+					textRes.innerHTML = 'Enter Credentials..  ';
+	var request = new XMLHttpRequest();
+
+	//capture response and store in variable 
+	request.onreadystatechange =function(){
+		if(request.readyState== XMLHttpRequest.DONE){
+		    
+			if(request.status===200){
+			    alert('User logIn successfully');
+					textRes.innerHTML = request.responseText;
+					
+			} else {
+			    alert('Invalid User');
+			    textRes.innerHTML = 'Error : '+request.status;
+			}
+		}
+	};
+	var username = document.getElementById("username").value;
+	var password = document.getElementById("password").value;
+	
+   console.log(username);
+   console.log(password);
+   console.log('login ');
+   	//make the request
+	request.open('POST','/login',true);
+	request.setRequestHeader('Content-Type','application/json');
+	request.send(JSON.stringify({username: username, password: password}));
+	
+};//login
+
 
 }
 
