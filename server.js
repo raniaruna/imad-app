5dd88b9845ce7a3012de503aaf2a78a930d64bc0
raniaruna <30811229+raniaruna@users.noreply.github.com>
2017-08-18 11:37:20 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -47,6 +47,8 @@ function createTemplate(data){
 		<div id="comments"><a  href='javascript:void(0)' onClick='loadComments(${title})'>Display Comments</a></div>
 		</div>
 		</div>
+		<script type="text/javascript" src="/ui/main.js">
+        </script>
     </body>
 </html>`;
 return htmlTemplate;
