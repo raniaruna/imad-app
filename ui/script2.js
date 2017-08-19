13ae153c09a5e4dbfef7318601700f4be6f7a8c0
raniaruna <30811229+raniaruna@users.noreply.github.com>
2017-08-18 17:01:07 -0700

    [imad-console] Updates ui/script2.js

--- ui/script2.js
+++ ui/script2.js
@@ -1,11 +1,40 @@
 console.log('Articles ..Loaded!');
 
-function loadCommentForm(){
+function loadCommentForm(articleName){
     var commentHTML =` <h3>Add Cooment </h3><input type='text' id='comment' placeholder='Add comment'/><br/>
                     
                     <input type='submit' id='comment_btn' value='Save'/>` ;
     var commentForm = document.getElementById("comment_form");
     commentForm.innerHTML = commentHTML;
+    
+    var submit = document.getElementById("comment_btn");
+    submit.onClick =function(){
+         var request = new XMLHttpRequest();
+    request.onreadystatechange =function(){
+if(request.readyState== XMLHttpRequest.DONE){
+    
+		   
+			if(request.status===200){
+			    
+			    var content = '<ul>';
+			    var commentData =JSON.parse(request.responseText);
+			    for(var i=0; i< commentData.length; i++){
+			        content +=`<li> ${commentData[i].comment} -(${commentData[i].timestamp.split('T')[0]})</li>`;
+			    }
+			    content +='</ul>';
+			    comment.innerHTML = content;
+			    console.log(request.responseText);
+			} else {
+			   comment.innerHTML ='Could not load all Comments!';
+			  
+			}
+		}
+    };
+    var comment =document.getElementById("comment");
+	request.open('POST','/submit-comment/'+articleName,true);
+	request.setRequestHeader('Content-Type','application/json');
+	request.send(JSON.stringify({comment:comment}));
+    };
     
 }
 function loadComments(articleName){
